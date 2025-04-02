1350ce70a3ff9459c124c1f17a55bd4161ac1998
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-04-02 12:09:38 +0200

    p

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		// Get rid of the CORS error
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "/",
 				changeOrigin: true,
 				secure: false,
 			},
